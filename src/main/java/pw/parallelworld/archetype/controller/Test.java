726354821b0a991b919fe855426cd230f6c5b855
parallelworld <A403afde2ca3f4b>
2019-04-16 20:42:07 +0800

    增加tomcat访问日志和业务日志打印

--- src/main/java/pw/parallelworld/archetype/controller/Test.java
+++ src/main/java/pw/parallelworld/archetype/controller/Test.java
@@ -1,16 +1,19 @@
 package pw.parallelworld.archetype.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pw.parallelworld.archetype.core.Result;
 
 @RestController
+@Slf4j
 public class Test {
 
     @RequestMapping(value = "/hello")
     public Result hello() throws Exception {
-        throw new Exception("异常啊异常");
-//        return Result.success("ahhaaha");
+//        throw new Exception("异常啊异常");
+        log.info("还行吧");
+        return Result.success("ahhaaha");
     }
 }
