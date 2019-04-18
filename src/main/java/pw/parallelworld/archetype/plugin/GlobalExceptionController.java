8e94e51408b2ffecff4ca5d015c150e691affb60
parallelworld <A403afde2ca3f4b>
2019-04-18 16:33:33 +0800

    增加4xx和5xx页面

--- src/main/java/pw/parallelworld/archetype/plugin/GlobalExceptionController.java
+++ src/main/java/pw/parallelworld/archetype/plugin/GlobalExceptionController.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.cglib.beans.BeanMap;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import pw.parallelworld.archetype.core.Result;
@@ -15,19 +15,24 @@ import java.util.Map;
 
 @Controller
 public class GlobalExceptionController extends BasicErrorController {
-    
+
     public GlobalExceptionController() {
         super(new DefaultErrorAttributes(), new ErrorProperties());
     }
 
     @Override
     public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
-        return super.errorHtml(request, response);
-    }
-
-    @Override
-    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
-        return super.error(request);
+        ModelAndView modelAndView = super.errorHtml(request, response);
+        HttpStatus status = getStatus(request);
+        modelAndView.addObject("httpStatus", status);
+        if (status != null) {
+            if (status.is4xxClientError()) {
+                modelAndView.setViewName("error/4xx");
+            } else if (status.is5xxServerError()) {
+                modelAndView.setViewName("error/5xx");
+            }
+        }
+        return modelAndView;
     }
 
     @SuppressWarnings("unchecked")
