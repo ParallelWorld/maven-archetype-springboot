b5ec52765b3c7449bb995f316a27e508ddebd4b4
parallelworld <A403afde2ca3f4b>
2019-04-23 12:26:35 +0800

    去掉远程debug

--- start_debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mvn clean package;
-nohup java -Xms64m -Xmx64m -Xss256k -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=5005,suspend=y -jar ./target/*.jar & nohup;
-tail -f nohup.out;
