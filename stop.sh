473ba757e96141f1ec27cb6f1610067dd24e27aa
parallelworld <A403afde2ca3f4b>
2019-04-23 12:06:55 +0800

    stop

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps -ef | grep vpn-server | grep -v grep | cut -c 9-15 | xargs kill -s 9;
+ps -ef | grep "/target/archetype-0.0.1-SNAPSHOT.jar" | grep -v grep | cut -c 9-15 | xargs kill -s 9;
